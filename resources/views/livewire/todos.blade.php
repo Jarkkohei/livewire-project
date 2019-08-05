5eee0fa4f86515e4abe626a448e25c09b2b5c4f9
Jarkko <jarkkohei@luukku.com>
2019-08-05 20:10:43 +0300

    Re-structure the Todos-component and fix responsiveness

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -21,25 +21,31 @@
 
     <ul class="list-group mt-4">
         @foreach($todos as $todo)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
+            <li class="list-group-item d-flex justify-content-between align-items-center py-2 px-3">
                 <div>
                     <input 
                         type="checkbox" 
-                        class="mr-4" 
+                        class="mr-3" 
                         wire:change="toggleCompleted({{ $todo->id }})" 
                         {{ $todo->completed ? 'checked' : '' }}
                     >
-                    <a 
-                        href="#" 
-                        style="{{ $todo->completed ? 'text-decoration: line-through' : '' }}"
-                        onclick="updateTodoPrompt('{{ $todo->title }}') || event.stopImmediatePropagation()"
-                        wire:click="updateTodo({{ $todo->id }}, updatedTodo)"
-                    >
-                        {{ $todo->title }}
-                    </a>
                 </div>
 
                 <div>
+                    <span style="{{ $todo->completed ? 'text-decoration: line-through' : '' }}">
+                        {{ $todo->title }}
+                    </span>
+                </div>
+
+                <div style="min-width: 60px; margin-left: 20px;">
+                    <button 
+                        class="btn btn-sm btn-primary" 
+                        onclick="updateTodoPrompt('{{ $todo->title }}') || event.stopImmediatePropagation()"
+                        wire:click="updateTodo({{ $todo->id }}, updatedTodo)"
+                    >
+                        E
+                    </button>
+
                     <button 
                         class="btn btn-sm btn-danger" 
                         onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
