5a99f05f740e7b204e25d4d37ab3e2746da9cb41
Jarkko <jarkkohei@luukku.com>
2019-08-06 21:13:05 +0300

    Extract addTodo -section into its own template-file

---
+++ resources/views/includes/addTodo.blade.php
@@ -0,0 +1,57 @@
+<div class="card d-flex flex-column justify-content-between">
+
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <div>
+            Add new todo
+        </div>
+    </div>
+
+    <div class="card-body">
+        <div class="form-group">
+            <label for="addTodoTitle">Title</label>
+            <input 
+                id="addTodoTitle"
+                name="addTodoTitle"
+                type="text"
+                class="form-control form-control-lg {{ $errors->has('title') ? 'border border-danger' : '' }}"
+                placeholder="Title..."
+                aria-describedby="titleErrors"
+                wire:model="title"
+            >
+
+            @if($errors->has('title'))
+                <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <label for="addTodoDescription">Description</label>
+            <textarea 
+                id="addTodoDescription"
+                name="addTodoDescription"
+                class="form-control form-control-lg {{ $errors->has('description') ? 'border border-danger' : $title }}"
+                placeholder="Description..."
+                rows="5"
+                {{--value="{{ $errors->has('description') ? old('addTodoDescription') : $description }}"--}}
+                value=""
+                wire:model="description"
+                aria-describedby="descriptionErrors"
+            ></textarea>
+
+            @if($errors->has('description'))
+                <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
+            @endif
+        </div>
+
+        <div class="form-group">
+            <button 
+                class="btn btn-primary form-control" 
+                wire:click="addTodo" 
+                type="submit"
+                title="Add new Todo"
+            >
+                Add
+            </button>
+        </div>
+    </div>
+</div>
--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,57 +1,5 @@
 <div>
-    <div class="card d-flex flex-column justify-content-between">
-
-        <div class="card-header">Add new todo</div>
-
-        <div class="card-body">
-            <div class="form-group">
-                <label for="addTodoTitle">Title</label>
-                <input 
-                    id="addTodoTitle"
-                    name="addTodoTitle"
-                    type="text"
-                    class="form-control form-control-lg {{ $errors->has('title') ? 'border border-danger' : '' }}"
-                    placeholder="Title..."
-                    value=""
-                    aria-describedby="titleErrors"
-                    wire:model="title"
-                >
-
-                @if($errors->has('title'))
-                    <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="addTodoDescription">Description</label>
-                <textarea 
-                    id="addTodoDescription"
-                    name="addTodoDescription"
-                    class="form-control form-control-lg {{ $errors->has('description') ? 'border border-danger' : $title }}"
-                    placeholder="Description..."
-                    rows="5"
-                    value="{{ $errors->has('description') ? old('addTodoDescription') : $description }}"
-                    wire:model="description"
-                    aria-describedby="descriptionErrors"
-                ></textarea>
-
-                @if($errors->has('description'))
-                    <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <button 
-                    class="btn btn-primary form-control" 
-                    wire:click="addTodo" 
-                    type="submit"
-                    title="Add new Todo"
-                >
-                    Add
-                </button>
-            </div>
-        </div>
-    </div>
+    @include('includes.addTodo')
 
     <div class="accordion  mt-4" id="todoAccordion">
         @foreach($todos as $todo)
