d0d06c33bcc1a7781b1b05d0803329435e1cfd16
Jarkko <jarkkohei@luukku.com>
2019-08-06 18:51:14 +0300

    Re-structure addTodo inputs and Add-button

--- resources/views/livewire/todos.blade.php
+++ resources/views/livewire/todos.blade.php
@@ -1,29 +1,57 @@
 <div>
-    <div class="input-group d-flex">
-        <input 
-            id="addTodo"
-            name="addTodo"
-            type="text"
-            class="form-control form-control-lg {{ $errors->has('title') ? 'border border-danger' : '' }}"
-            placeholder="New todo..."
-            value="{{ old('addTodo') }}"
-            wire:model="title"
-        >
-        <div class="input-group-append">
-            <button 
-                class="btn btn-primary" 
-                wire:click="addTodo" 
-                type="submit"
-                title="Add new Todo"
-            >
-                Add
-            </button>
+    <div class="card d-flex flex-column justify-content-between">
+
+        <div class="card-header">Add new todo</div>
+
+        <div class="card-body">
+            <div class="form-group">
+                <label for="addTodoTitle">Title</label>
+                <input 
+                    id="addTodoTitle"
+                    name="addTodoTitle"
+                    type="text"
+                    class="form-control form-control-lg {{ $errors->has('title') ? 'border border-danger' : '' }}"
+                    placeholder="Title..."
+                    value=""
+                    aria-describedby="titleErrors"
+                    wire:model="title"
+                >
+
+                @if($errors->has('title'))
+                    <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <label for="addTodoDescription">Description</label>
+                <textarea 
+                    id="addTodoDescription"
+                    name="addTodoDescription"
+                    class="form-control form-control-lg {{ $errors->has('description') ? 'border border-danger' : $title }}"
+                    placeholder="Description..."
+                    rows="5"
+                    value="{{ $errors->has('description') ? old('addTodoDescription') : $description }}"
+                    wire:model="description"
+                    aria-describedby="descriptionErrors"
+                ></textarea>
+
+                @if($errors->has('description'))
+                    <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
+                @endif
+            </div>
+
+            <div class="form-group">
+                <button 
+                    class="btn btn-primary form-control" 
+                    wire:click="addTodo" 
+                    type="submit"
+                    title="Add new Todo"
+                >
+                    Add
+                </button>
+            </div>
         </div>
     </div>
-
-    @if($errors->has('title'))
-        <div class="text-danger">{{ $errors->first('title') }}</div>
-    @endif
 
     <div class="accordion  mt-4" id="todoAccordion">
         @foreach($todos as $todo)
