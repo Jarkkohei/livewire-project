ddf0468e0880aac286e731681073204020d2b865
Jarkko <jarkkohei@luukku.com>
2019-08-12 00:07:55 +0300

    Home-view height to 100 vh

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-10 col-lg-8">
+        <div class="col-md-10 col-lg-8 vh-100">
              This is home view
         </div>
     </div>
