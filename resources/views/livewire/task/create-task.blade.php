aa68429cd3aa882738352e8372072d7273b7183a
Jarkko <jarkkohei@luukku.com>
2019-08-12 08:30:41 +0300

    Re-style edit- and create-view layouts

--- resources/views/livewire/task/create-task.blade.php
+++ resources/views/livewire/task/create-task.blade.php
@@ -1,70 +1,74 @@
 <div class="container vh-100">
-    <div class="card shadow-sm">
+    <div class="row justify-content-center">
+        <div class="col col-md-8 col-lg-6">
+            <div class="card shadow-sm">
 
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <div>
-                Add new task
-            </div>        
-            <div>
-                <button 
-                    class="btn btn-sm btn-outline-secondary"
-                    wire:click="back"
-                    title="go back"
-                >
-                    <i class="fas fa-arrow-left"></i> Back
-                </button>
-            </div>
-        </div>
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <div>
+                        Add new task
+                    </div>        
+                    <div>
+                        <button 
+                            class="btn btn-sm btn-outline-secondary"
+                            wire:click="back"
+                            title="go back"
+                        >
+                            <i class="fas fa-arrow-left"></i> Back
+                        </button>
+                    </div>
+                </div>
 
-        <div class="card-body">
-            <div class="form-group">
-                <label for="addTaskTitle">Title</label>
-                <small class="text-danger">(required)</small>
-                <input 
-                    id="addTaskTitle"
-                    name="addTaskTitle"
-                    type="text"
-                    class="form-control {{ $errors->has('title') ? 'border border-danger' : '' }} shadow-sm"
-                    placeholder="Title..."
-                    aria-describedby="titleErrors"
-                    wire:model="title"
-                    autocomplete="off"
-                    autofocus
-                >
+                <div class="card-body">
+                    <div class="form-group">
+                        <label for="addTaskTitle">Title</label>
+                        <small class="text-danger">(required)</small>
+                        <input 
+                            id="addTaskTitle"
+                            name="addTaskTitle"
+                            type="text"
+                            class="form-control {{ $errors->has('title') ? 'border border-danger' : '' }} shadow-sm"
+                            placeholder="Title..."
+                            aria-describedby="titleErrors"
+                            wire:model="title"
+                            autocomplete="off"
+                            autofocus
+                        >
 
-                @if($errors->has('title'))
-                    <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
-                @endif
-            </div>
+                        @if($errors->has('title'))
+                            <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
+                        @endif
+                    </div>
 
-            <div class="form-group">
-                <label for="addTaskDescription">Description</label>
-                <textarea 
-                    id="addTaskDescription"
-                    name="addTaskDescription"
-                    class="form-control {{ $errors->has('description') ? 'border border-danger' : '' }} shadow-sm"
-                    placeholder="Description..."
-                    rows="5"
-                    value=""
-                    wire:model="description"
-                    aria-describedby="descriptionErrors"
-                    autocomplete="off"
-                ></textarea>
+                    <div class="form-group">
+                        <label for="addTaskDescription">Description</label>
+                        <textarea 
+                            id="addTaskDescription"
+                            name="addTaskDescription"
+                            class="form-control {{ $errors->has('description') ? 'border border-danger' : '' }} shadow-sm"
+                            placeholder="Description..."
+                            rows="5"
+                            value=""
+                            wire:model="description"
+                            aria-describedby="descriptionErrors"
+                            autocomplete="off"
+                        ></textarea>
 
-                @if($errors->has('description'))
-                    <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
-                @endif
-            </div>
+                        @if($errors->has('description'))
+                            <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
+                        @endif
+                    </div>
 
-            <div class="form-group">
-                <button 
-                    class="btn btn-success form-control" 
-                    wire:click="create" 
-                    type="submit"
-                    title="Add new Task"
-                >
-                    Add
-                </button>
+                    <div class="form-group">
+                        <button 
+                            class="btn btn-success form-control" 
+                            wire:click="create" 
+                            type="submit"
+                            title="Add new Task"
+                        >
+                            Add
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/livewire/task/edit-task.blade.php
+++ resources/views/livewire/task/edit-task.blade.php
@@ -1,93 +1,97 @@
 <div class="container vh-100">
-    <div class="card shadow-sm">
+    <div class="row justify-content-center">
+        <div class="col col-md-8 col-lg-6">
+            <div class="card shadow-sm">
 
-        <div class="card-header d-flex justify-content-between align-items-center">
-            <div>
-                Edit task
-            </div>
-            <div>
-                <button 
-                    class="btn btn-sm btn-outline-secondary"
-                    wire:click="back"
-                    title="go back"
-                >
-                    <i class="fas fa-arrow-left"></i> Back
-                </button>
-            </div>
-        </div>
-
-        <div class="card-body">
-            <div class="form-group ">
-                <label for="editTaskTitle">Title</label>
-                <small class="text-danger">(required)</small>
-                <input 
-                    id="editTaskTitle"
-                    name="editTaskTitle"
-                    type="text"
-                    class="form-control form-control {{ $errors->has('title') ? 'border border-danger' : '' }} shadow-sm"
-                    placeholder="Title..."
-                    aria-describedby="titleErrors"
-                    autocomplete="off"
-                    wire:model="title"
-                >
-
-                @if($errors->has('title'))
-                    <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="editTaskDescription">Description</label>
-                <textarea 
-                    id="editTaskDescription"
-                    name="editTaskDescription"
-                    class="form-control {{ $errors->has('description') ? 'border border-danger' : '' }} shadow-sm"
-                    placeholder="Description..."
-                    rows="5"
-                    value=""
-                    wire:model="description"
-                    aria-describedby="descriptionErrors"
-                    autocomplete="off"
-                ></textarea>
-
-                @if($errors->has('description'))
-                    <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
-                @endif
-            </div>
-
-            <div class="form-group">
-                <label for="editTaskStatus">Status</label>
-                <select 
-                    id="editTaskStatus" 
-                    name="editTaskStatus"  
-                    class="form-control form-control shadow-sm"
-                    wire:model="status" 
-                >
-                    @foreach($taskStatuses as $taskStatus)
-                        <option 
-                            name="{{ $taskStatus['label'] }}" 
-                            value="{{ $taskStatus['value'] }}" 
-                            {{ $status == $taskStatus['value'] ? 'selected' : '' }}
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <div>
+                        Edit task
+                    </div>
+                    <div>
+                        <button 
+                            class="btn btn-sm btn-outline-secondary"
+                            wire:click="back"
+                            title="go back"
                         >
-                            {{ $taskStatus['label'] }}
-                        </option>
-                    @endforeach
-                </select>
+                            <i class="fas fa-arrow-left"></i> Back
+                        </button>
+                    </div>
+                </div>
 
-                @if($errors->has('status'))
-                    <small id="statusErrors" class="form-text text-danger">{{ $errors->first('status') }}</small>
-                @endif
-            </div>
+                <div class="card-body">
+                    <div class="form-group ">
+                        <label for="editTaskTitle">Title</label>
+                        <small class="text-danger">(required)</small>
+                        <input 
+                            id="editTaskTitle"
+                            name="editTaskTitle"
+                            type="text"
+                            class="form-control form-control {{ $errors->has('title') ? 'border border-danger' : '' }} shadow-sm"
+                            placeholder="Title..."
+                            aria-describedby="titleErrors"
+                            autocomplete="off"
+                            wire:model="title"
+                        >
 
-            <div class="form-group">
-                <button 
-                    class="btn btn-success form-control" 
-                    wire:click="update" 
-                    type="submit"
-                    title="Save task"
-                >
-                    Save
-                </button>
+                        @if($errors->has('title'))
+                            <small id="titleErrors" class="form-text text-danger">{{ $errors->first('title') }}</small>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label for="editTaskDescription">Description</label>
+                        <textarea 
+                            id="editTaskDescription"
+                            name="editTaskDescription"
+                            class="form-control {{ $errors->has('description') ? 'border border-danger' : '' }} shadow-sm"
+                            placeholder="Description..."
+                            rows="5"
+                            value=""
+                            wire:model="description"
+                            aria-describedby="descriptionErrors"
+                            autocomplete="off"
+                        ></textarea>
+
+                        @if($errors->has('description'))
+                            <small id="descriptionErrors" class="form-text text-danger">{{ $errors->first('description') }}</small>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <label for="editTaskStatus">Status</label>
+                        <select 
+                            id="editTaskStatus" 
+                            name="editTaskStatus"  
+                            class="form-control form-control shadow-sm"
+                            wire:model="status" 
+                        >
+                            @foreach($taskStatuses as $taskStatus)
+                                <option 
+                                    name="{{ $taskStatus['label'] }}" 
+                                    value="{{ $taskStatus['value'] }}" 
+                                    {{ $status == $taskStatus['value'] ? 'selected' : '' }}
+                                >
+                                    {{ $taskStatus['label'] }}
+                                </option>
+                            @endforeach
+                        </select>
+
+                        @if($errors->has('status'))
+                            <small id="statusErrors" class="form-text text-danger">{{ $errors->first('status') }}</small>
+                        @endif
+                    </div>
+
+                    <div class="form-group">
+                        <button 
+                            class="btn btn-success form-control" 
+                            wire:click="update" 
+                            type="submit"
+                            title="Save task"
+                        >
+                            Save
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
