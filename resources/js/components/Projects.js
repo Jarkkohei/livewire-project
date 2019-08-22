6773fd7c8ec612c6ad497752133736fce28738a6
Jarkko <jarkkohei@luukku.com>
2019-08-22 08:52:25 +0300

    Show Projects children if it has any

--- resources/js/components/Projects.js
+++ resources/js/components/Projects.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { setCurrentProject } from '../actions/projects';
 
-const Projects = ({ match }) => {
+const Projects = (props) => {
 
     const projects = useSelector(state => state.projects.projects);
     const currentProject = useSelector(state => state.projects.currentProject);
@@ -17,19 +17,43 @@ const Projects = ({ match }) => {
             </div>
 
             <ul className="list-group list-group-flush">
-                {projects.map(project => (
-                    <NavLink 
-                        className="list-group-item projectsListItem" 
-                        to={ `/projects/${project.id}`} 
-                        title={project.description}
-                        key={project.id}
-                    >
-                        <span>{project.title}</span>
-                    </NavLink>
+                {projects.map(project => project.level == 1 && (
+                    <ProjectsListItem project={project} key={project.id}/>
                 ))}
             </ul>
 
         </div> 
+    );
+}
+
+const ProjectsListItem = ({ project }) => {
+
+    const styles = { paddingLeft: project.level * 10 + 10 };
+
+    return (
+        <>
+            <NavLink
+                style={styles}
+                className="list-group-item projectsListItem d-flex align-items-center justify-content-between"
+                to={`/projects/${project.id}`}
+                title={project.description}
+                key={project.id}
+            >
+                <span>{project.title}</span>
+                {project.children.length > 0 ?
+                    (project.showChildren ? (
+                        <i className="fas fa-caret-down"></i>
+                    ) : (
+                            <i className="fas fa-caret-right"></i>
+                        )
+                    ) : ''}
+            </NavLink>
+
+            {project.children.length > 0 ? project.children.map(child => (
+                <ProjectsListItem project={child} key={child.id} />
+            ))
+             : ''}
+        </>
     );
 }
 
