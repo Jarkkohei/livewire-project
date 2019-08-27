3ac102287ec841b278b2a9d0005a1fac2bf7e6f2
Jarkko <jarkkohei@luukku.com>
2019-08-27 23:46:47 +0300

    Create a Spinner -component for indicating loading state

---
+++ resources/js/components/Spinner.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const Spinner = ({ 
+    icon = 'fas fa-spinner',
+    color = 'inherit', 
+    size = '2rem', 
+    speed = '1s', 
+    timing = 'linear' 
+}) => {
+
+    const StyledSpinner = styled.i`
+        font-size: ${size};
+        color: ${color};
+
+        animation: ${spin} ${speed} ${timing} infinite;
+    `;
+
+    return (
+        <StyledSpinner className={icon}/>
+    )
+}
+
+export default Spinner;
+
+export const spin = keyframes`
+    100% {
+        transform: rotate(360deg);
+    }
+`;
