aa72f92b9a404ab92a2ecabca9b93c58d7aa4bb0
Jarkko <jarkkohei@luukku.com>
2019-08-29 11:28:52 +0300

    Fix Sorting -component inner layout

--- resources/js/components/Sorting.js
+++ resources/js/components/Sorting.js
@@ -4,41 +4,33 @@ const Sorting = ({ currentSortOptionId, options, setSortValues }) => {
 
     return (
         <div className="card shadow-sm mt-3">
-            <div className="card-header d-flex justify-content-between align-items-center flex-column flex-sm-row flex-lg-column px-0">
+            <div className="card-header d-flex justify-content-between align-items-center flex-column flex-sm-row flex-lg-column px-3">
 
-                <div className="form-group col mb-0">
-
-                    <div className="row justify-content-start align-items-center">
-                        <div className="col-12 col-sm-auto">
-                            <label htmlFor="sortBySelect">
-                                Order by:
-                            </label>
-                        </div>
-
-                        <div className="col-12 col-sm-auto">
-                            <select
-                                value={currentSortOptionId}
-                                onChange={setSortValues}
-                                name="sortBySelect"
-                                className="form-control form-control-sm shadow-sm"
+                <div className="form-group col mb-0 px-1">
+                    <label htmlFor="sortBySelect">
+                        Order by:
+                    </label>
+                
+                    <select
+                        value={currentSortOptionId}
+                        onChange={setSortValues}
+                        name="sortBySelect"
+                        className="form-control form-control-sm shadow-sm"
+                    >
+                        {options && options.map((opt) => (
+                            <option
+                                key={opt.id}
+                                name={opt.label}
+                                value={opt.id}
                             >
-                                {options && options.map((opt) => (
-                                    <option
-                                        key={opt.id}
-                                        name={opt.label}
-                                        value={opt.id}
-                                    >
-                                        {opt.label}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                    </div>
-
+                                {opt.label}
+                            </option>
+                        ))}
+                    </select>
                 </div>
 
             </div >
-        </div >
+        </div>
     );
 }
 
