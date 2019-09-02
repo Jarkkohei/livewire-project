0dfd2a02191b80e569230c16dd6853ee9f060e24
Jarkko <jarkkohei@luukku.com>
2019-09-02 12:14:40 +0300

    Fix Sorting -segment height to match Filtering -segment height

--- resources/js/components/Sorting.js
+++ resources/js/components/Sorting.js
@@ -4,7 +4,7 @@ const Sorting = ({ currentSortOptionId, options, setSortValues }) => {
 
     return (
         <div className="card shadow-sm mt-3">
-            <div className="card-header d-flex justify-content-between align-items-center flex-column flex-sm-row flex-lg-column px-3">
+            <div className="card-header d-flex justify-content-between align-items-center flex-column flex-sm-row flex-lg-column px-3" style={{ minHeight: 86 }}>
 
                 <div className="form-group col mb-0 px-1">
                     <label htmlFor="sortBySelect">
