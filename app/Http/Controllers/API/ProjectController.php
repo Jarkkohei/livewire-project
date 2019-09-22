06e3a7ee91147edcd0a6f5df5b7d356f4832329e
Jarkko <jarkkohei@luukku.com>
2019-09-22 08:03:09 +0300

    Fix the middleware to be used in the future with API authentucation

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -17,7 +17,7 @@ class ProjectController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        //$this->middleware('auth:api');
     }
 
     /**
